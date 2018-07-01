386d4da12901a20e429681ef415b2d1b84d2f9d8
MY PC <MY PC@192.168.1.105>
2018-07-01 19:06:58 -0400

    program for password generation

--- password_generation/src/password_generation/Password_generation.java
+++ password_generation/src/password_generation/Password_generation.java
@@ -1,4 +1,8 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package password_generation;
 
 import java.util.Random;
@@ -48,7 +52,11 @@ public class Password_generation {
                return password;
         }
 
-        
+       
           
     }
-   
+    
+
+    
+    
+
